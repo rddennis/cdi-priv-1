db72a8db906dfcc236dbd1acb5e368f4f5a482ff
Ronesha Dennis <ronesha_dennis@yahoo.com>
2026-02-24 22:42:21 -0500

    change app port

--- app.py
+++ app.py
@@ -248,4 +248,5 @@ Return JSON only.
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
